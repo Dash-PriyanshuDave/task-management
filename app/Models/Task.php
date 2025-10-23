d4e23034c426cf2772ed5b24b6ec5901655be74c
Priyanshu Dave <priyanshu.d@dashtechinc.com>
2025-10-23 19:49:18 +0530

    Completed database setup: models, migrations, relationships

---
+++ app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable =[
+        'title',
+        'description',
+        'completed',
+        'project_id',
+        'assigned_to'
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function assignee()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -44,5 +45,20 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class, 'created_by');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'assigned_to');
+    }
+
+    public function isManager()
+    {
+        return $this->role === 'manager';
     }
 }
